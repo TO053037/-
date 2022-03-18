ffc55005c6741f78cd0708bcae5df5bfb9008bff
TO053037 <67772806+TO053037@users.noreply.github.com>
2022-03-18 12:29:38 +0900

    find insident centering picture

--- Javascript/springSwitch.js
+++ Javascript/springSwitch.js
@@ -4,10 +4,10 @@ const springBtn = document.querySelector('#spring-switch');
 springBtn.addEventListener('click', () => {
     const pictureList = document.getElementById('spring').querySelectorAll("li");
     for (let i = 0; i < pictureList.length; i++) {
-        if (pictureList[i].querySelector('img').className === 'show') {
-            pictureList[i].querySelector('img').className = 'not-show'
-        } else if (pictureList[i].querySelector('img').className === 'not-show'){
-            pictureList[i].querySelector('img').className = 'show'
+        if (pictureList[i].className === 'show') {
+            pictureList[i].className = 'not-show'
+        } else if (pictureList[i].className === 'not-show'){
+            pictureList[i].className = 'show'
         }
     }
 
